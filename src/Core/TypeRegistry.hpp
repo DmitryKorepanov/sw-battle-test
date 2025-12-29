da9a310bf23225da2e79b2c11eb4353a130a7975
Дмитрий Корепанов <dmitry.korepanov@playrix.com>
2025-12-29 13:20:58 +0200

    TypeRegistry refactoring: correct pointer save, add comments, type normalization, change static_assert -> requires, add [[nodiscard]] and noexcept

--- src/Core/TypeRegistry.hpp
+++ src/Core/TypeRegistry.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
+#include <concepts>
 #include <memory>
-#include <typeindex>
-#include <unordered_map>
-#include <vector>
-#include <utility>
 #include <stdexcept>
+#include <string>
 #include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 namespace sw::core
 {
@@ -14,8 +17,60 @@ namespace sw::core
 	class TypeRegistry
 	{
 	private:
-		std::unordered_map<std::type_index, std::shared_ptr<void>> _instances;
+		template <typename T>
+		[[nodiscard]] static std::type_index typeIndex() noexcept
+		{
+			// Normalize type to avoid accidental duplication for const/ref-qualified queries like get<const T>().
+			using TNormalized = std::remove_cvref_t<T>;
+			return std::type_index(typeid(TNormalized));
+		}
+
+		struct Entry
+		{
+			std::shared_ptr<void> instance;
+			std::type_index owner;
+		};
+
+		std::unordered_map<std::type_index, Entry> _instances;
 		std::unordered_map<std::type_index, std::vector<std::type_index>> _aliases;
+
+		void removeAliases(const std::type_index& owner)
+		{
+			if (auto it = _aliases.find(owner); it != _aliases.end())
+			{
+				for (const auto& alias : it->second)
+				{
+					// Sanity check: ensure we are removing an alias that still belongs to this owner
+					if (auto entry = _instances.find(alias); entry != _instances.end() && entry->second.owner == owner)
+					{
+						_instances.erase(entry);
+					}
+				}
+				_aliases.erase(it);
+			}
+		}
+
+		template <typename Interface>
+		void checkAliasCollision(const std::type_index& owner) const
+		{
+			auto aliasType = typeIndex<Interface>();
+			if (auto it = _instances.find(aliasType); it != _instances.end() && it->second.owner != owner)
+			{
+				throw std::runtime_error(
+					std::string("TypeRegistry alias collision: interface '") + typeid(Interface).name()
+					+ "' is already registered by '" + it->second.owner.name() + "'");
+			}
+		}
+
+		template <typename T, typename Interface>
+		void registerInterface(const std::shared_ptr<T>& instance, const std::type_index& owner)
+		{
+			auto aliasType = typeIndex<Interface>();
+			// CRITICAL: Store shared_ptr<Interface> erased to void.
+			// This ensures the void* points to the Interface subobject, not the Concrete object start.
+			std::shared_ptr<Interface> interfacePtr = instance;
+			_instances.insert_or_assign(aliasType, Entry{ interfacePtr, owner });
+		}
 
 	public:
 		TypeRegistry() = default;
@@ -27,69 +82,89 @@ namespace sw::core
 		TypeRegistry& operator=(TypeRegistry&&) = default;
 
 		template <typename T, typename... Interfaces, typename... Args>
-		std::shared_ptr<T> emplace(Args&&... args)
+		requires(std::derived_from<T, Interfaces> && ...)
+		[[nodiscard]] std::shared_ptr<T> emplace(Args&&... args)
 		{
-			static_assert((std::is_base_of_v<Interfaces, T> && ...), "T must inherit from all specified interfaces");
-
 			auto instance = std::make_shared<T>(std::forward<Args>(args)...);
-			auto typeT = std::type_index(typeid(T));
-			
-			_instances[typeT] = instance;
-			
-			if constexpr (sizeof...(Interfaces) > 0)
-			{
-				((_instances[std::type_index(typeid(Interfaces))] = instance), ...);
-				_aliases[typeT] = { std::type_index(typeid(Interfaces))... };
-			}
-			
+			add<T, Interfaces...>(instance);
 			return instance;
 		}
 
 		template <typename T, typename... Interfaces>
+		requires(std::derived_from<T, Interfaces> && ...)
 		void add(std::shared_ptr<T> instance)
 		{
-			static_assert((std::is_base_of_v<Interfaces, T> && ...), "T must inherit from all specified interfaces");
+			if (!instance)
+			{
+				throw std::invalid_argument("TypeRegistry::add: instance must not be null");
+			}
 
-			auto typeT = std::type_index(typeid(T));
-			_instances[typeT] = instance;
-			
+			const auto ownerType = typeIndex<T>();
+
+			// 1. Check for collisions early to fail fast on configuration errors.
+			(checkAliasCollision<Interfaces>(ownerType), ...);
+
+			// 2. Clear old aliases if we are updating an existing object
+			removeAliases(ownerType);
+
+			// 3. Register concrete type
+			_instances.insert_or_assign(ownerType, Entry{ instance, ownerType });
+
+			// 4. Register interfaces
 			if constexpr (sizeof...(Interfaces) > 0)
 			{
-				((_instances[std::type_index(typeid(Interfaces))] = instance), ...);
-				_aliases[typeT] = { std::type_index(typeid(Interfaces))... };
+				// Track aliases owned by this concrete type so remove() can clean them up.
+				_aliases[ownerType] = { typeIndex<Interfaces>()... };
+
+				(registerInterface<T, Interfaces>(instance, ownerType), ...);
 			}
 		}
 
 		template <typename T>
-		std::shared_ptr<T> get() const
+		[[nodiscard]] std::shared_ptr<T> get()
 		{
-			auto it = _instances.find(std::type_index(typeid(T)));
-			if (it != _instances.end())
+			if (auto it = _instances.find(typeIndex<T>()); it != _instances.end())
 			{
-				return std::static_pointer_cast<T>(it->second);
+				return std::static_pointer_cast<T>(it->second.instance);
 			}
 			return nullptr;
 		}
 
 		template <typename T>
+		[[nodiscard]] std::shared_ptr<const T> get() const
+		{
+			if (auto it = _instances.find(typeIndex<T>()); it != _instances.end())
+			{
+				return std::static_pointer_cast<const T>(it->second.instance);
+			}
+			return nullptr;
+		}
+
+		template <typename T>
+		[[nodiscard]] bool contains() const noexcept
+		{
+			return _instances.contains(typeIndex<T>());
+		}
+
+		template <typename T>
 		void remove()
 		{
-			// NOTE: This method must be called with the CONCRETE type used during registration.
-			// Removing by interface type (if registered as such) will NOT clear the concrete instance 
-			// or other aliases due to lack of reverse-lookup.
-			auto typeT = std::type_index(typeid(T));
-			_instances.erase(typeT);
-			
-			// Remove aliases
-			auto it = _aliases.find(typeT);
-			if (it != _aliases.end())
+			const auto type = typeIndex<T>();
+			auto it = _instances.find(type);
+			if (it == _instances.end())
 			{
-				for (const auto& alias : it->second)
-				{
-					_instances.erase(alias);
-				}
-				_aliases.erase(it);
+				return;
 			}
+
+			const auto owner = it->second.owner;
+
+			// Optimization: if T is the concrete type, we can erase 'it' directly.
+			if (type == owner)
+				_instances.erase(it);
+			else
+				_instances.erase(owner);
+
+			removeAliases(owner);
 		}
 	};
 }
